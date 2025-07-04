6a032ad46f6eafbba2a2bbfdd11317183d125cb6
MarianaKochinski2025 <mariana.kochinski@escola.pr.gov.br>
2025-07-04 14:12:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Programação',
+    'O que é Arduino?',
+    'É uma plataforma de prototipagem eletrônica.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Brasil?',
+    'A capital do brasil é o Brasília'
+)
+criaCartao(
+    'Ciências',
+    'Da onde que vem a obsidiana?',
+    'Atravez da lava do vulcão'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Amor em Inglês?',
+    'Se diz Love'
+)
